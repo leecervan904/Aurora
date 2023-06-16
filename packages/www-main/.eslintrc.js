ce4c99e01cc8e5d108a6a7bd6368737ff57edb49
leecervan904 <zwlife93@gmail.com>
2023-06-16 13:17:12 +0800

    refactor: remove unnecessary files

---
+++ packages/www-main/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: ['../../.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/space-before-blocks': 'off',
+  },
+}
